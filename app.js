1a12b0d763e2a219690f12aa1d44a85b56e62b15
AaronHoffman <aahoffm@gmail.com>
2021-08-20 18:05:02 -0400

    fixed error handling

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ app.get("/api/:date", function(req, res) {
 const getDate = (input=null) => {
     if (input !== null) {
         const date = new Date(input);
-        return getObject(date);
+        if (date.toString() === 'Invalid Date') {
+            return {error: 'Invalid Date'}
+        } else { 
+            return getObject(date);
+        }    
     } else {
         const date = new Date();
         return getObject(date);
     }
 }
+
 
 const getObject = (date) => {
     const responseObject = {
